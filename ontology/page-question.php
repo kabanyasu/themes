95f8d48d1a71fd9f6d54f5dee472489df813e8e1
sayu349 <106569098+sayu349@users.noreply.github.com>
2022-10-12 15:00:44 +0900

    より適切な書き方へ変更
    - お作法的な部分の手直し。
    - 基本ifとwhileで囲んで表示してあげた方がいいらしいので、それに準じて変更しました。

--- ontology/page-question.php
+++ ontology/page-question.php
@@ -6,12 +6,17 @@
 
     </div>
     <div class="question-form">
-        <h2>
-            <?php the_title(); ?>
-        </h2>
-        <div class="question-form-text">
-            <?php the_content(); ?>
-        </div>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) :
+                the_post(); ?>
+                <h2>
+                    <?php the_title(); ?>
+                </h2>
+                <div class="question-form-text">
+                    <?php the_content(); ?>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 </main>
 <?php get_footer(); ?>
--- ontology/page-thanks.php
+++ ontology/page-thanks.php
@@ -1,7 +1,12 @@
 <main class="main">
     <div class="title">
-        <h1><?php the_title(); ?></h1>
-        <p><?php the_content(); ?></p>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) :
+                the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <p><?php the_content(); ?></p>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 </main>
 <?php get_footer(); ?>
