aba3ffcfa21c5c240961d1a1f4bdaae53151ec1c
sayu349 <106569098+sayu349@users.noreply.github.com>
2022-10-19 13:55:02 +0900

    ロゴ変更
    主要箇所のロゴを変更しました。

--- ontology/header.php
+++ ontology/header.php
@@ -11,7 +11,7 @@
     <header class="header">
         <div class="header-inner">
             <a class="header-logo" href="<?php echo esc_url(home_url()); ?>">
-                <img src="<?php get_template_directory_uri(); ?>/wp-content/themes/ontology/images/common/logo-header.png" alt="会社ロゴ">
+                <img src="<?php get_template_directory_uri(); ?>/wp-content/themes/ontology/images/inagaki_office_icon/logo.png" alt="会社ロゴ">
             </a>
             <button class="toggle-menu-button"></button>
             <div class="header-site-menu">
